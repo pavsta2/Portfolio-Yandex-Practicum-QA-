b403e5322617934549c56d733c7a76ad34b274c2
pavelstarshinov@gmail.com <pav$sta2>
2023-11-27 20:04:52 +0300

    modified 27.11

--- sirius_autotests_selenium_pytest/pages/reg_page.py
+++ sirius_autotests_selenium_pytest/pages/reg_page.py
@@ -5,30 +5,32 @@ from selenium.webdriver.support.ui import Select
 # Регистрируем класс, который будет описывать вэб страницу
 class RegPage:
     # Размечаем элементы страницы:
-    FIRSTNAME_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div//div[3]//label//div[2]//input')
-    LASTNAME_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div//div[2]//label//div[2]//input')
-    REG_BUTTON = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/button')
-    PATRONYMIC_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div/div//div[4]//label//div[2]//input')
-    BIRTH_DATE = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[5]/label/div[2]/div/div/div/input')
-    EMAIL_FIELD = (By.XPATH, '//*[@id="index"]//div/div[2]//div[4]//div//div//div[6]//label//div[2]//input')
-    VOSH_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div//div[7]//label//div[2]//input')
-    PHONE_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div/div[8]//label//div[2]//input')
-    SNILS_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div//div[9]//label//div[2]//input')
-    SNILS_FIELD_GENERIC = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[9]/label/div[2]')
-    PROF_FIELD = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[10]/label/div[2]/input')
-    COUNTRY_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div//div[11]//div//div[2]//label//div[2]//select')
-    CITY_FIELD = (By.XPATH, '//*[@id="index"]//div//div[2]//div[4]//div//div//div[11]//div//div[3]//label//div[2]//input')
-    ORG_FIELD = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[11]/div/div[4]/label/div[2]/input')
-    SCHOOL_FIELD = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[11]/div/div[5]/label/div[2]/input')
-    GRADE_FIELD = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[11]/div/div[6]/label/div[2]/input')
-    RADIOBUTT_MAIN_OLIMP = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[12]/ul/li[1]/span[1]')
-    RADIOBUTT_ADIT_OLIMP = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[12]/ul/li[2]/span[1]')
-    CHECK_CONF_INP_DATA = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[13]/div/label/input')
-    CHECK_ACC_USR_AGRMNT = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[14]/div/label/input')
-    CHECK_RULES = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[15]/div/label/input')
-    SNILS_ERR_MESS_XPATH = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[9]/div')
-    USR_AGRMNT_LNK = (By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[14]/div/div/span/p/a[1]')
-    SUCC_REG_MSG = (By.XPATH, '//*[@id="index"]/div/div[2]/div/div[2]')
+    locators = {
+        'FIRSTNAME_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div//div[3]//label//div[2]//input',
+        'LASTNAME_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div//div[2]//label//div[2]//input',
+        'REG_BUTTON': '//*[@id="index"]/div/div[2]/div[4]/button',
+        'PATRONYMIC_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div/div//div[4]//label//div[2]//input',
+        'BIRTH_DATE': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[5]/label/div[2]/div/div/div/input',
+        'EMAIL_FIELD': '//*[@id="index"]//div/div[2]//div[4]//div//div//div[6]//label//div[2]//input',
+        'VOSH_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div//div[7]//label//div[2]//input',
+        'PHONE_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div/div[8]//label//div[2]//input',
+        'SNILS_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div//div[9]//label//div[2]//input',
+        'SNILS_FIELD_BORDER': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[9]/label/div[2]',
+        'PROF_FIELD': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[10]/label/div[2]/input',
+        'COUNTRY_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div//div[11]//div//div[2]//label//div[2]//select',
+        'CITY_FIELD': '//*[@id="index"]//div//div[2]//div[4]//div//div//div[11]//div//div[3]//label//div[2]//input',
+        'ORG_FIELD': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[11]/div/div[4]/label/div[2]/input',
+        'SCHOOL_FIELD': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[11]/div/div[5]/label/div[2]/input',
+        'GRADE_FIELD': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[11]/div/div[6]/label/div[2]/input',
+        'RADIOBUTT_MAIN_OLIMP': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[12]/ul/li[1]/span[1]',
+        'RADIOBUTT_ADIT_OLIMP': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[12]/ul/li[2]/span[1]',
+        'CHECK_CONF_INP_DATA': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[13]/div/label/input',
+        'CHECK_ACC_USR_AGRMNT': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[14]/div/label/input',
+        'CHECK_RULES': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[15]/div/label/input',
+        'SNILS_ERR_MESS_XPATH': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[9]/div',
+        'USR_AGRMNT_LNK': '//*[@id="index"]/div/div[2]/div[4]/div/div/div[14]/div/div/span/p/a[1]',
+        'SUCC_REG_MSG': '//*[@id="index"]/div/div[2]/div/div[2]'
+        }
 
     SNILS_FIELD_ERR_CLASSNAME = ("has-outline-w text-s ui-input-wrapper ui-input-wrapper-mode-wrong ui-"
                                  "input-wrapper-size-s   ui-textinput ui-schema-auth-form__input  box box-orient-"
@@ -37,22 +39,15 @@ class RegPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def enter_firstname(self, firstname: str) -> None:
-        """Функция для заполнения поля Имя"""
-        self.driver.find_element(*self.FIRSTNAME_FIELD).send_keys(firstname)
+    def fill_the_field(self, fields_to_fill: list, test_data: dict) -> None:
+        """Функция для заполнения поля"""
+        for item in fields_to_fill:
+            self.driver.find_element(By.XPATH, self.locators[item]).send_keys(test_data[item])
 
-    def enter_patronymic(self, patronymic: str) -> None:
-        """Функция для заполнения поля Отчество"""
-        self.driver.find_element(*self.PATRONYMIC_FIELD).send_keys(patronymic)
-
-    def enter_lastname(self, lastname: str) -> None:
-        """Функция для заполнения поля Фамилия"""
-        self.driver.find_element(*self.LASTNAME_FIELD).send_keys(lastname)
-
-    def enter_birthdate(self, birthdate: str) -> None:
+    def enter_birthdate(self) -> None:
         """Функция для заполнения поля Дата рождения"""
         # Кликаем по полю:
-        self.driver.find_element(*self.BIRTH_DATE).click()
+        self.driver.find_element(By.XPATH, self.locators['BIRTH_DATE']).click()
         # Объект поля выбора года во всплывающем календаре записываем в переменную Select
         select = Select(self.driver.find_element(By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[5]/label/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div/select'))
         # Выбираем из списка 1981 года
@@ -60,103 +55,27 @@ class RegPage:
         # Кликаем на день календаря, чтобы дата отобразилась в поле Дата рождения
         self.driver.find_element(By.XPATH, '//*[@id="index"]/div/div[2]/div[4]/div/div/div[5]/label/div[2]/div[2]/div[2]/div/div/div[2]/div[2]/div[4]/div[5]').click()
 
-    def enter_email(self, email: str) -> None:
-        """Функция для заполнения поля Email"""
-        self.driver.find_element(*self.EMAIL_FIELD).send_keys(email)
-
-    def enter_vosh(self, vosh: str) -> None:
-        """Функция для заполнения поля Вош-номер"""
-        self.driver.find_element(*self.VOSH_FIELD).send_keys(vosh)
-
-    def enter_phone(self, phone: str) -> None:
-        """Функция для заполнения поля Телефон"""
-        self.driver.find_element(*self.PHONE_FIELD).send_keys(phone)
-
-    def enter_snils(self, snils: str) -> None:
-        """Функция для заполнения поля СНИЛС"""
-        self.driver.find_element(*self.SNILS_FIELD).send_keys(snils)
-
-    def enter_prof(self, prof: str) -> None:
-        """Функция для заполнения поля Профессия"""
-        self.driver.find_element(*self.PROF_FIELD).send_keys(prof)
-
     def enter_country(self, val: str) -> None:
         """Функция для заполнения поля Страна"""
-        select = Select(self.driver.find_element(*self.COUNTRY_FIELD))
+        select = Select(self.driver.find_element(By.XPATH, self.locators['COUNTRY_FIELD']))
         select.select_by_value(val)
 
-    def enter_city(self, city: str) -> None:
-        """Функция для заполнения поля Город"""
-        self.driver.find_element(*self.CITY_FIELD).send_keys(city)
-
-    def enter_org(self, org: str) -> None:
-        """Функция для заполнения поля Организация"""
-        self.driver.find_element(*self.ORG_FIELD).send_keys(org)
-
-    def enter_school(self, school: str) -> None:
-        """Функция для заполнения поля Школа"""
-        self.driver.find_element(*self.SCHOOL_FIELD).send_keys(school)
-
-    def enter_grade(self, grade: str) -> None:
-        """Функция для заполнения поля Класс"""
-        self.driver.find_element(*self.GRADE_FIELD).send_keys(grade)
-
-    def click_radio_but_main_olimpiada(self) -> None:
+    def click_element(self, elem_locs: list) -> None:
         """Функция для клика по радиокнопке Основная олимпиада"""
-        self.driver.find_element(*self.RADIOBUTT_MAIN_OLIMP).click()
+        for elem in elem_locs:
+            self.driver.find_element(By.XPATH, self.locators[elem]).click()
 
-    def click_radio_but_adit_olimpiada(self) -> None:
-        """Функция для клика по радиокнопке Дополнительная олимпиада"""
-        self.driver.find_element(*self.RADIOBUTT_ADIT_OLIMP).click()
-
-    def click_check_conf_inp_data(self) -> None:
-        """Функция для клика по чек боксу Я подтверждаю правильность указанных данных"""
-        self.driver.find_element(*self.CHECK_CONF_INP_DATA).click()
-
-    def click_check_acc_usr_agrmnt(self) -> None:
-        """Функция для клика по чек боксу Я принимаю..."""
-        self.driver.find_element(*self.CHECK_ACC_USR_AGRMNT).click()
-
-    def click_check_rules(self) -> None:
-        """Функция для клика по чек боксу Я прочитал..."""
-        self.driver.find_element(*self.CHECK_RULES).click()
-
-    def reg_button(self):
+    def get_elem_obj(self, elem_key: str):
         """Функция для получения объекта кнопки Перейти к тестированию"""
-        return self.driver.find_element(*self.REG_BUTTON)
+        return self.driver.find_element(By.XPATH, self.locators[elem_key])
 
-    def click_reg_button(self) -> None:
-        """Функция для клика по кнопке Перейти к тестированию"""
-        self.driver.find_element(*self.REG_BUTTON).click()
-
-    def find_snils_field_generic(self):
-        """Функция для получения объекта элемента страницы с рамкой поля СНИЛС"""
-        return self.driver.find_element(*self.SNILS_FIELD_GENERIC)
-
-    def find_snils_field(self):
-        """Функция для получения объекта элемента с текстом поля СНИЛС"""
-        return self.driver.find_element(*self.SNILS_FIELD)
-
-    def find_snils_err_mess(self):
-        """Функция для получения объекта элемента с ошибкой ввода данных СНИЛС"""
-        err_mess = None
+    def find_elem_or_ex(self, elem_loc: str):
+        """Функция для получения объекта элемента"""
+        elem = None
         try:
-            err_mess = self.driver.find_element(*self.SNILS_ERR_MESS_XPATH)
+            elem = self.driver.find_element(By.XPATH, self.locators[elem_loc])
         except:
             print("Element is not found on the page!")
 
-        return err_mess
+        return elem
 
-    def click_usr_agrmnt_lnk(self) -> None:
-        """Функция для клика по ссылке Пользовательское соглашение"""
-        self.driver.find_element(*self.USR_AGRMNT_LNK).click()
-
-    def find_succ_reg_mess(self):
-        """Функция для получения объекта элемента с текстом об удачной регистрации"""
-        succ_reg_mess = None
-        try:
-            succ_reg_mess = self.driver.find_element(*self.SUCC_REG_MSG)
-        except:
-            print("Element is not found on the page!")
-
-        return succ_reg_mess
--- sirius_autotests_selenium_pytest/tests/test_reg_page.py
+++ sirius_autotests_selenium_pytest/tests/test_reg_page.py
@@ -3,15 +3,30 @@ import pytest
 import time
 from conftest import browser
 
-
 # Импортируем класс LoginPage из файла login_page.py
 from pages.reg_page import RegPage
 
 
+# Создаем словарь с локаторами полей и валидными тестовыми данными
+data_fields = {
+        'FIRSTNAME_FIELD': "test_user",
+        'LASTNAME_FIELD': "test_lastname",
+        'EMAIL_FIELD': 'pavel@gmail.com',
+        'SNILS_FIELD': '07721418864',
+        'PROF_FIELD': 'teacher',
+        'CITY_FIELD': "spb",
+        'ORG_FIELD': 'test org',
+        'SCHOOL_FIELD': '123',
+        'GRADE_FIELD': "11",
+        'COUNTRY': 'AB'
+    }
+
 # Определяем функции-тесты, с подробным описанием в doc string
-# использованы декораторы для добавления тегов и возможности запуска тестов по тэгам
+# использованы декоратор для добавления тегов и возможности запуска тестов по тэгам
+# используем декоратор для передачи параметров тестирования
 @pytest.mark.reg_button_tests
-def test_is_reg_butt_enabl_if_all_corr_filled(browser):
+@pytest.mark.parametrize("data_fields", [(data_fields)])
+def test_is_reg_butt_enabl_if_all_corr_filled(browser, data_fields):
     """При корр заполнении всех полей КРОМЕ "Отчество", "ВОШ-логин", "Телефон", а также активации всех
     чек-боксов и одной радиокнопки, кнопка "Перейти к тестированию" становится активной."""
 
@@ -19,29 +34,32 @@ def test_is_reg_butt_enabl_if_all_corr_filled(browser):
     reg_page = RegPage(browser)
     # Открываем страницу с помощью веб-драйвера
     browser.get("https://uts.sirius.online//#/auth/register/qainternship")
+
     # Заполняем поля
-    reg_page.enter_firstname("test_user")
-    reg_page.enter_lastname("test_lastname")
-    reg_page.enter_birthdate('26.02.1981')
-    reg_page.enter_email('pavel@gmail.com')
-    reg_page.enter_snils('07721418864')
-    reg_page.enter_prof('teacher')
-    reg_page.enter_city("spb")
-    reg_page.enter_org('test org')
-    reg_page.enter_prof('test')
-    reg_page.enter_school('123')
-    reg_page.enter_grade("11")
-    reg_page.click_check_acc_usr_agrmnt()
-    reg_page.click_check_conf_inp_data()
-    reg_page.click_check_rules()
-    reg_page.enter_country('AB')
+    reg_page.fill_the_field(['FIRSTNAME_FIELD',
+                             'LASTNAME_FIELD',
+                             'EMAIL_FIELD',
+                             'SNILS_FIELD',
+                             'PROF_FIELD',
+                             'CITY_FIELD',
+                             'ORG_FIELD',
+                             'SCHOOL_FIELD',
+                             'GRADE_FIELD'],
+                            data_fields)
+    reg_page.enter_birthdate()
+    reg_page.enter_country(data_fields['COUNTRY'])
+    # Кликаем на все чек боксы
+    reg_page.click_element(['CHECK_ACC_USR_AGRMNT',
+                            'CHECK_CONF_INP_DATA',
+                            'CHECK_RULES'])
     # Проверяем, что кнопка активна
-    assert reg_page.reg_button().is_enabled() == True
+    assert reg_page.get_elem_obj('REG_BUTTON').is_enabled() == True
     browser.close()
 
 
 @pytest.mark.reg_button_tests
-def test_make_success_registration(browser):
+@pytest.mark.parametrize("data_fields", [(data_fields)])
+def test_make_success_registration(browser, data_fields):
     """Если все необходимые поля формы корр заполнены, радиокнопки и чек боксы активированы, то при нажатии на
     кнопку "Перейти к тестированию" появляется сообщение с информацией о том, что ссылка на задание отправлена
     на email, на указанный при регистрации."""
@@ -51,32 +69,33 @@ def test_make_success_registration(browser):
     # Открываем страницу с помощью веб-драйвера
     browser.get("https://uts.sirius.online//#/auth/register/qainternship")
     # Заполняем обязат поля
-    reg_page.enter_firstname("test_user")
-    reg_page.enter_lastname("test_lastname")
-    reg_page.enter_birthdate('26.02.1981')
-    reg_page.enter_email('test@test.ru')
-    reg_page.enter_snils('07721418864')
-    reg_page.enter_prof('teacher')
-    reg_page.enter_city("spb")
-    reg_page.enter_org('test org')
-    reg_page.enter_prof('test')
-    reg_page.enter_school('123')
-    reg_page.enter_grade("11")
-    reg_page.click_check_acc_usr_agrmnt()
-    reg_page.click_check_conf_inp_data()
-    reg_page.click_check_rules()
-    reg_page.enter_country('AB')
-    # Нажимаем кнопку регистрации
-    reg_page.reg_button().click()
+    reg_page.fill_the_field(['FIRSTNAME_FIELD',
+                             'LASTNAME_FIELD',
+                             'EMAIL_FIELD',
+                             'SNILS_FIELD',
+                             'PROF_FIELD',
+                             'CITY_FIELD',
+                             'ORG_FIELD',
+                             'SCHOOL_FIELD',
+                             'GRADE_FIELD'],
+                            data_fields)
+    reg_page.enter_birthdate()
+    reg_page.enter_country(data_fields['COUNTRY'])
+    # Кликаем все чек боксы и кнопку регистрации
+    reg_page.click_element(['CHECK_ACC_USR_AGRMNT',
+                            'CHECK_CONF_INP_DATA',
+                            'CHECK_RULES',
+                            'REG_BUTTON'])
     # Ищем и получаем объект элемента с сообщением об успешной регистрации
-    success_mess = str(reg_page.find_succ_reg_mess().get_attribute('textContent'))
+    success_mess = str(reg_page.find_elem_or_ex('SUCC_REG_MSG').get_attribute('textContent'))
     # Проверяем, что пользователь видит в сообщении свой email
-    assert success_mess.find('test@test.ru') != -1
+    assert success_mess.find(data_fields['EMAIL_FIELD']) != -1
     browser.close()
 
 
 @pytest.mark.reg_button_tests
-def test_is_reg_butt_disabl_if_not_firstname(browser):
+@pytest.mark.parametrize("data_fields", [(data_fields)])
+def test_is_reg_butt_disabl_if_not_firstname(browser, data_fields):
     """Если все поля формы корр заполнены КРОМЕ поля "Имя" (не заполнено), кнопка
     "Перейти к тестированию" НЕ активна."""
 
@@ -85,27 +104,29 @@ def test_is_reg_butt_disabl_if_not_firstname(browser):
     # Открываем страницу с помощью веб-драйвера
     browser.get("https://uts.sirius.online//#/auth/register/qainternship")
     # Заполняем поля
-    reg_page.enter_lastname("test_password")
-    reg_page.enter_birthdate('26.02.1981')
-    reg_page.enter_email('pavel@gmail.com')
-    reg_page.enter_snils('07721418864')
-    reg_page.enter_prof('teacher')
-    reg_page.enter_city("spb")
-    reg_page.enter_org('test org')
-    reg_page.enter_prof('test')
-    reg_page.enter_school('123')
-    reg_page.enter_grade("11")
-    reg_page.click_check_acc_usr_agrmnt()
-    reg_page.click_check_conf_inp_data()
-    reg_page.click_check_rules()
-    reg_page.enter_country('AB')
+    reg_page.fill_the_field(['LASTNAME_FIELD',
+                             'EMAIL_FIELD',
+                             'SNILS_FIELD',
+                             'PROF_FIELD',
+                             'CITY_FIELD',
+                             'ORG_FIELD',
+                             'SCHOOL_FIELD',
+                             'GRADE_FIELD'],
+                            data_fields)
+    reg_page.enter_birthdate()
+    reg_page.enter_country(data_fields['COUNTRY'])
+    # Кликаем все чек боксы
+    reg_page.click_element(['CHECK_ACC_USR_AGRMNT',
+                            'CHECK_CONF_INP_DATA',
+                            'CHECK_RULES'])
     # Проверяем, что кнопка не активна
-    assert reg_page.reg_button().is_enabled() == False
+    assert reg_page.get_elem_obj('REG_BUTTON').is_enabled() == False
     browser.close()
 
 
 @pytest.mark.reg_button_tests
-def test_is_reg_butt_disabl_if_not_lastname(browser):
+@pytest.mark.parametrize("data_fields", [(data_fields)])
+def test_is_reg_butt_disabl_if_not_lastname(browser, data_fields):
     """Если все поля формы корректно заполнены КРОМЕ поля "Фамилия" (не заполнено), кнопка
     "Перейти к тестированию" НЕ активна."""
 
@@ -114,29 +135,30 @@ def test_is_reg_butt_disabl_if_not_lastname(browser):
     # Открываем страницу с помощью веб-драйвера
     browser.get("https://uts.sirius.online//#/auth/register/qainternship")
     # Заполняем поля
-    reg_page.enter_firstname("test_user")
-    reg_page.enter_birthdate('26.02.1981')
-    reg_page.enter_email('pavel@gmail.com')
-    reg_page.enter_snils('07721418864')
-    reg_page.enter_prof('teacher')
-    reg_page.enter_city("spb")
-    reg_page.enter_org('test org')
-    reg_page.enter_prof('test')
-    reg_page.enter_school('123')
-    reg_page.enter_grade("11")
-    reg_page.click_check_acc_usr_agrmnt()
-    reg_page.click_check_conf_inp_data()
-    reg_page.click_check_rules()
-    reg_page.enter_country('AB')
+    reg_page.fill_the_field(['FIRSTNAME_FIELD',
+                             'EMAIL_FIELD',
+                             'SNILS_FIELD',
+                             'PROF_FIELD',
+                             'CITY_FIELD',
+                             'ORG_FIELD',
+                             'SCHOOL_FIELD',
+                             'GRADE_FIELD'],
+                            data_fields)
+    reg_page.enter_birthdate()
+    reg_page.enter_country(data_fields['COUNTRY'])
+    # Кликаем все чек боксы
+    reg_page.click_element(['CHECK_ACC_USR_AGRMNT',
+                            'CHECK_CONF_INP_DATA',
+                            'CHECK_RULES'])
     # Проверяем, что кнопка не активна
-    assert reg_page.reg_button().is_enabled() == False
+    assert reg_page.get_elem_obj('REG_BUTTON').is_enabled() == False
     browser.close()
 
 
 # Данная функция обернута в декоратор для обеспечения параметризации
 # В данном тесте валидации используется три варианта невалидных данных
 @pytest.mark.validation_tests
-@pytest.mark.parametrize('snils', ['test', 'тест_по_русски', '@#$%^'])
+@pytest.mark.parametrize('snils', [{'SNILS_FIELD': 'test'}, {'SNILS_FIELD': 'тест_по_русски'}, {'SNILS_FIELD': '@#$%^'}])
 def test_fill_snils_with_let(browser, snils):
     """Если в поле "СНИЛС" ввести НЕ цифру, то появляется красная рамка и сообщение красным шрифтом "СНИЛС
     должен содержать только цифры"."""
@@ -145,12 +167,12 @@ def test_fill_snils_with_let(browser, snils):
     reg_page = RegPage(browser)
     # Открываем страницу с помощью веб-драйвера
     browser.get("https://uts.sirius.online//#/auth/register/qainternship")
-    # Заполняем поле некорректными данными
-    reg_page.enter_snils(snils)
+    # Заполняем поле некорректными данными из декоратора параметризации
+    reg_page.fill_the_field(['SNILS_FIELD'], snils)
     # Проверяем className у элемента поля с рамкой
-    assert reg_page.find_snils_field_generic().get_attribute('className') == reg_page.SNILS_FIELD_ERR_CLASSNAME
+    assert reg_page.find_elem_or_ex('SNILS_FIELD_BORDER').get_attribute('className') == reg_page.SNILS_FIELD_ERR_CLASSNAME
     # Проверяем, что появился текст с ошибкой и сам текст
-    assert reg_page.find_snils_err_mess().get_attribute('outerText') == "СНИЛС должен содержать только цифры"
+    assert reg_page.find_elem_or_ex('SNILS_ERR_MESS_XPATH').get_attribute('outerText') == "СНИЛС должен содержать только цифры"
     browser.close()
 
 
@@ -164,7 +186,7 @@ def test_open_usr_agrmnt_link(browser):
     # Открываем страницу с помощью веб-драйвера
     browser.get("https://uts.sirius.online//#/auth/register/qainternship")
     # Кликаем по ссылке
-    reg_page.click_usr_agrmnt_lnk()
+    reg_page.click_element(['USR_AGRMNT_LNK'])
     # Получаем объект со всеми вкладками
     handles = browser.window_handles
     # Переходим на вторую вкладку
